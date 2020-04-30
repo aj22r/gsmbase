90a482b82b1a15707f36b2ce1872351565c76965
aj22r <arturj22r@hot.ee>
2020-04-30 21:44:45 +0300

    Remove unneccesary

--- CMakeFiles/lss.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/lss"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/lss.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
